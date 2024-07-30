74b569807d049236eb7ed6daf8455ec634e68563
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-07-29 18:09:45 -0600

    plantillas
    CREAR PLANTILLAS

--- index.py
+++ index.py
@@ -1,18 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask (__name__)
 
-@app.route('/')
+""" @app.route('/')
 def principal():
     return "Bienvenidos o bienvenida a mi sitio web con PYTHON"
 
 @app.route('/contacto')
 def contacto():
-    return "Esta es la página de contactos"
+    return "Esta es la página de contactos HOY"
 
 @app.route('/lenguajeprogramacion')
 def lenguajes():
-    return "Esta es la pagina de lenguajes de programación"
+    return "Esta es la pagina de lenguajes de programación" """
+    
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

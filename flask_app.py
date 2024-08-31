dd0aae8d6517594472e650bf1cc812a0e24146df
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-08-30 18:58:35 -0600

    d

---
+++ flask_app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for
+
+app = Flask (__name__)
+
+""" @app.route('/')
+def principal():
+    return "Bienvenidos o bienvenida a mi sitio web con PYTHON"
+
+@app.route('/contacto')
+def contacto():
+    return "Esta es la página de contactos HOY"
+
+@app.route('/lenguajeprogramacion')
+def lenguajes():
+    return "Esta es la pagina de lenguajes de programación" """
+    
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html') 
+
+@app.route('/lenguaje')
+def lenguaje():
+    return render_template('lenguaje.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

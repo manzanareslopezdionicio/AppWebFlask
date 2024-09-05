2f53cc0a1c029b05951fad783cdf9b1ecad1cf1d
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-09-05 16:14:00 -0600

    login

--- app.py
+++ app.py
@@ -4,15 +4,24 @@ app = Flask (__name__)
    
 @app.route('/')
 def home():
-    return render_template('inicio.html')
+    return render_template('index.html')
 
 @app.route('/servicios')
 def servicios():
     return render_template('servicios.html') 
-'''
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
 @app.route('/lenguaje')
 def lenguaje():
     return render_template('lenguaje.html')
-'''
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)

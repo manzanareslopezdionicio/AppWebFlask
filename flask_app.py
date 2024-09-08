e0ccf9f1a38c125946522ca8b4faac70835a25a4
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-09-07 22:41:45 -0600

    regis

--- flask_app.py
+++ flask_app.py
@@ -1,30 +1,30 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask (__name__)
-
-""" @app.route('/')
-def principal():
-    return "Bienvenidos o bienvenida a mi sitio web con PYTHON"
-
-@app.route('/contacto')
-def contacto():
-    return "Esta es la página de contactos HOY"
-
-@app.route('/lenguajeprogramacion')
-def lenguajes():
-    return "Esta es la pagina de lenguajes de programación" """
-    
+   
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html') 
+
 @app.route('/contacto')
 def contacto():
-    return render_template('contacto.html') 
+    return render_template('contacto.html')
 
 @app.route('/lenguaje')
 def lenguaje():
     return render_template('lenguaje.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

bd1c1c6d58e94059a327c9ae9d1718d36a234816
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-10-20 20:42:32 -0600

    git

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def insertar():
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO cliente(name,email,phone) VALUES(%s, %s, %s)", (name, email, phone))
         mysql.connection.commit()
-        flash('Agregado satisfactoriamente', 'success')
+        flash('Agregado satisfactoriamente', 'success') # MENSAJE DE ALERTA
         cur.close()
         return redirect(url_for('cliente'))
 
